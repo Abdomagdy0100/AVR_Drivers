9e8bfb679595807dc5ccdcc417b679546a5b9929
Abd-el-rahaman Magdy <96621447+Abdomagdy0100@users.noreply.github.com>
2022-02-04 00:01:52 +0200

    Delete HAL_LCD directory

--- HAL_LCD/Debug/LCD_prog.d
+++
@@ -1,8 +0,0 @@
-LCD_prog.o LCD_prog.o: ../LCD_prog.c ../LIB/types.h ../DIO_int.h \
-  ../LCD_int.h
-
-../LIB/types.h:
-
-../DIO_int.h:
-
-../LCD_int.h:
--- HAL_LCD/Debug/main.d
+++
@@ -1,7 +0,0 @@
-main.o main.o: ../main.c ../LIB/types.h ../DIO_int.h ../LCD_int.h
-
-../LIB/types.h:
-
-../DIO_int.h:
-
-../LCD_int.h:
