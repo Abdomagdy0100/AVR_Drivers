7a28b5b106d22a67fe202be9cef35692bbdcfae6
Abd-el-rahaman Magdy <96621447+Abdomagdy0100@users.noreply.github.com>
2022-02-03 15:39:31 +0200

    Update DIO_prog.c

--- MCAL_Interrupt/DIO_prog.c
+++ MCAL_Interrupt/DIO_prog.c
@@ -1,9 +1,9 @@
-/*
- * DIO_prog.c
- *
- *  Created on: Aug 11, 2018
- *      Author: seif
- */
+/******************************************************/
+/* Author     : ABDO_MAGDY_AHMED                      */
+/* Group ID   : 0120082101			      */
+/* Student ID : 012008210103			      */
+/* Subject    : Interfacing_DIO                       */
+/******************************************************/
 
 #include "LIB/STD_TYPES.h"
 #include "LIB/BIT_MATH.h"
